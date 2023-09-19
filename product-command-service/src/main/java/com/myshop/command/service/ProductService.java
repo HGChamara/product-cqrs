b67c2e7da52b4c55ab86d02e58a33dd877068f7a
HGChamara <hgchamara1991@gmail.com>
2023-09-19 08:22:42 +0530

    add kafka

--- product-command-service/src/main/java/com/myshop/command/service/ProductService.java
+++ product-command-service/src/main/java/com/myshop/command/service/ProductService.java
@@ -1,27 +1,35 @@
 package com.myshop.command.service;
 
+import com.myshop.dto.ProductEvent;
 import com.myshop.command.entity.Product;
 import com.myshop.command.repository.ProductRepository;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
-
-    public ProductService(ProductRepository productRepository) {
+    private KafkaTemplate<String, Object> kafkaTemplate;
+    public ProductService(ProductRepository productRepository, KafkaTemplate kafkaTemplate) {
         this.productRepository = productRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public Product createProduct(Product product) {
-        return productRepository.save(product);
+        Product productDO = productRepository.save(product);
+        ProductEvent event = new ProductEvent("CreateProduct", product);
+        kafkaTemplate.send("product-event-topic", event);
+        return productDO;
     }
 
     public Product updateProduct(Long id, Product updatedProduct) {
         Product product = productRepository.findById(id).get();
-
         product.setAmount(updatedProduct.getAmount());
         product.setName(updatedProduct.getName() != null ? updatedProduct.getName() : product.getName());
         product.setDescription(updatedProduct.getDescription() != null ? updatedProduct.getDescription() : product.getDescription());
+
+        ProductEvent event = new ProductEvent("UpdateProduct", product);
+        kafkaTemplate.send("product-event-topic", event);
         return productRepository.save(product);
     }
 
--- product-query-service/src/main/java/com/myshop/query/service/ProductQueryService.java
+++ product-query-service/src/main/java/com/myshop/query/service/ProductQueryService.java
@@ -1,7 +1,9 @@
 package com.myshop.query.service;
 
+import com.myshop.dto.ProductEvent;
 import com.myshop.query.entity.Product;
 import com.myshop.query.repository.ProductRepository;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,5 +18,19 @@ public class ProductQueryService {
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @KafkaListener(topics = "product-event-topic", groupId = "group2")
+    public void processProductsEvent(ProductEvent event) {
+        if(event.getEventType().equals("CreateProduct")) {
+            productRepository.save(event.getProduct());
+        } else if(event.getEventType().equals("UpdateProduct")) {
+            Product updatedProduct = event.getProduct();
+            Product product = productRepository.findById(updatedProduct.getId()).get();
+            product.setAmount(updatedProduct.getAmount());
+            product.setName(updatedProduct.getName());
+            product.setDescription(updatedProduct.getDescription());
+            productRepository.save(product);
+        }
     }
 }
